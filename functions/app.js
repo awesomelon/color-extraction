c38cc7b5e1ca177fce8b97ec59875963abb96a62
방재호 <jhb@marpple.com>
2024-07-18 15:52:57 +0900

    .

--- functions/app.js
+++ functions/app.js
@@ -1,55 +1,8 @@
 import express, { Router } from "express";
-import multer from "multer";
-import path from "path";
-import fs from "fs";
-import { ColorExtractor } from "../core.js";
 import serverless from "serverless-http";
 
-const app = express();
+export const app = express();
 const router = Router();
-
-function ensureUploadDirExists(uploadDir) {
-  if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir);
-  }
-}
-
-function initMulter(uploadDir) {
-  return multer({
-    storage: multer.diskStorage({
-      destination: function (req, file, cb) {
-        cb(null, uploadDir);
-      },
-      filename: function (req, file, cb) {
-        cb(null, Date.now() + path.extname(file.originalname)); // 파일 확장자를 유지
-      },
-    }),
-  });
-}
-
-async function handleFileUpload(req, res) {
-  try {
-    const colorExtractor = ColorExtractor.getInstance();
-    const { colors, dominantColor } = await colorExtractor.extractColors(
-      req.file.path,
-    );
-    fs.unlink(req.file.path, (err) => {
-      if (err) {
-        console.error("Failed to delete file:", err);
-      }
-    });
-    res.json({ colors, dominantColor });
-  } catch (err) {
-    console.error("Error:", err);
-    res.status(500).send("Error processing image");
-  }
-}
-
-const uploadDir = "uploads";
-ensureUploadDirExists(uploadDir);
-const upload = initMulter(uploadDir);
-
-router().post("/upload", upload.single("image"), handleFileUpload);
 
 app.use("/.netlify/functions/app", router);
 
---
+++ server-local.js
@@ -0,0 +1,58 @@
+import { app } from "./functions/app.js";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
+import multer from "multer";
+import { ColorExtractor } from "./core.js";
+const port = 3000;
+
+function ensureUploadDirExists(uploadDir) {
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+}
+
+function initMulter(uploadDir) {
+  return multer({
+    storage: multer.diskStorage({
+      destination: function (req, file, cb) {
+        cb(null, uploadDir);
+      },
+      filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname)); // 파일 확장자를 유지
+      },
+    }),
+  });
+}
+
+async function handleFileUpload(req, res) {
+  try {
+    const colorExtractor = ColorExtractor.getInstance();
+    const { colors, dominantColor } = await colorExtractor.extractColors(
+      req.file.path,
+    );
+    fs.unlink(req.file.path, (err) => {
+      if (err) {
+        console.error("Failed to delete file:", err);
+      }
+    });
+    res.json({ colors, dominantColor });
+  } catch (err) {
+    console.error("Error:", err);
+    res.status(500).send("Error processing image");
+  }
+}
+
+const uploadDir = "uploads";
+ensureUploadDirExists(uploadDir);
+const upload = initMulter(uploadDir);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(dirname(fileURLToPath(import.meta.url)), "demo.html"));
+});
+
+app.post("/upload", upload.single("image"), handleFileUpload);
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
